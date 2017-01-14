ce0bd770803c1866e6ca64711e089e1d23ba6f4e
Seed Zeng <cdzengpeiyun@gmail.com>
2017-01-13 22:13:32 -0500

    FInished 253

---
+++ 72_Edit_Distance.py
@@ -0,0 +1,11 @@
+"""
+(a)(Complete Choice): for every problem instance, at least one
+choice is consistent with an optimal solution.
+(b) (Inductive Structure): for each initial choice ck, show that
+making choice c leaves one or more strictly smaller subproblems
+with no external constraints relative to ck.
+(c) (Optimal Substructure): for each initial choice ck, if its induced
+subproblems are solved optimally, then combining their
+solutions with choice ck yields a solution that is optimal among
+all solutions that make choice ck.
+"""
