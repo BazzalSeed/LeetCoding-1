1858169c477cb9c133f84ed19fa24c2ec8ecd249
Seed Zeng <cdzengpeiyun@gmail.com>
2017-01-14 23:02:42 -0500

    finished 72

--- Facebook/72_Edit_Distance.py
+++ Facebook/72_Edit_Distance.py
@@ -8,4 +8,75 @@ with no external constraints relative to ck.
 subproblems are solved optimally, then combining their
 solutions with choice ck yields a solution that is optimal among
 all solutions that make choice ck.
+
+Key is to find the recurrence function
+
+Description
+______________
+Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2. (each operation is counted as 1 step.)
+
+You have the following 3 operations permitted on a word:
+
+a) Insert a character
+b) Delete a character
+c) Replace a character
+
+Approach
+________________
+Define X[i....j] and Y[i.....j] as the two substrings of X and Y
+Define D(i, j) as the minimum distance between X and Y to the index i and j
+
+eg. X = abcd  Y = abcde
+
+  | "" |a |b |
+--|----|--|--|---------
+""|    |  |  |
+--|----|--|--|--------
+a |    |  |  |
+--|----|--|--|---------
+Start at empty string
+
+
+Initialization/Base Case
+=====================
+
+D(i,0) = i
+D(0, j)= j
+
+recurrence
+=====================
+D(i,j) = min (
+
+        D(i-1, j) + 1,
+        D(i, j-1) + 1,
+        D(i-1, j-1) (+1 if X[i] == X[j] else 0)
+)
+
+
 """
+
+
+class Solution(object):
+
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        n = len(word2) + 1
+        m = len(word1) + 1
+        edit = [[0 for _ in xrange(n)] for _ in xrange(m)]
+        for i in xrange(m):
+            for j in xrange(n):
+                if i == 0:
+                    edit[i][j] = j
+                elif j == 0:
+                    edit[i][j] = i
+                else:
+                    if word1[i - 1] == word2[j - 1]:
+                        cost = 0
+                    else:
+                        cost = 1
+                    edit[i][j] = min(edit[i - 1][j] + 1, edit[i][j - 1] + 1, edit[i - 1][j - 1] + cost)
+        return edit[m - 1][n - 1]
---
+++ Planner.md
@@ -0,0 +1,2 @@
+# 311	Sparse Matrix Multiplication 	50.6%	Medium
+# 314	Binary Tree Vertical Order Traversal
